0f766284d20a61aa25354899b2c57c71dbd8dfb7
abhirt2024 <abhishekart2087@gmail.com>
2025-12-28 14:01:45 +0530

    add.py python file added

---
+++ add.py
@@ -0,0 +1,6 @@
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+num1 = int(num1_str)
+num2 = int(num2_str)
+sum_of_numbers = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum_of_numbers}")
