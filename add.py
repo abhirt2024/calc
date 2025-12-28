033c4d0886efcb3f8e0a6c918ba3255e737a6509
abhirt2024 <abhishekart2087@gmail.com>
2025-12-28 14:08:32 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,5 +2,5 @@ num1_str = input("Enter the first number: ")
 num2_str = input("Enter the second number: ")
 num1 = int(num1_str)
 num2 = int(num2_str)
-sum_of_numbers = num1 + num2
-print(f"The sum of {num1} and {num2} is {sum_of_numbers}")
+mul_of_numbers = num1 * num2
+print(f"The mul of {num1} and {num2} is {mul_of_numbers}")
